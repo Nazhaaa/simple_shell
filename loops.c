99571c782908d950668d0ad7b631322328b6b3c3
Nazhaaa <nazhakhatib3@gmail.com>
2023-08-22 15:50:54 -0700

    simple shell

--- loops.c
+++ loops.c
@@ -1,3 +1,4 @@
+#include "main.h"
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
---
+++ main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#define STDOUT_FILEND 1
+int main (void);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif 
