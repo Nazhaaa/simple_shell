8177485a38f180c0be388502bec0f30ebb559a26
Nazhaaa <nazhakhatib3@gmail.com>
2023-08-23 19:07:09 -0700

    ddd

--- main.c
+++ main.c
@@ -48,4 +48,3 @@ break;
 }
 return (0);
 }
-
--- main.h
+++ main.h
@@ -6,10 +6,5 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define STDOUT_FILEND 1
-char *_getenv (const char *name);
-int exit_main(void);
-int shell_main(void);
 int pompt_main(void);
-unsigned int check_delim(char c, const char *str);
-char *_strtok(char *stri, const char *del);
 #endif
