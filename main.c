96825e933b3a20c41225a4dd3f871ad97a9b6357
Nazhaaa <nazhakhatib3@gmail.com>
2023-08-24 12:06:01 -0700

    simple

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
- * pompt_main - Entry point of the interactive shell program.
+ * main - Entry point of the interactive shell program.
  * This program implements a simple interactive shell that displays
  * a command prompt, reads user input, processes the entered command,
  * and waits for the command to complete before prompting for the next one.
@@ -17,6 +17,8 @@
  * Read: Number of characters read from input.
  * pid: Process ID of child process.
  * stat: Status of child process.
+ * argIndex:index
+ * token:token
  * Return: Always 0 (Success).
  */
 int main(void)
@@ -24,6 +26,8 @@ int main(void)
 char *buffer = NULL, *arg[] = {NULL, NULL};
 size_t siz;
 int Read, pid, stat;
+int argIndex = 0;
+char *token;
 while (1)
 {
 write(STDOUT_FILEND, "$ ", 2);
@@ -31,12 +35,24 @@ Read = getline(&buffer, &siz, stdin);
 if (Read >= 0)
 {
 buffer[Read - 1] = '\0';
-arg[0] = buffer;
+token = strtok(buffer, " ");
+while (token != NULL)
+{
+arg[argIndex++] = token;
+token = strtok(NULL, " ");
+}
+arg[argIndex] = NULL;
 pid = fork();
 if (pid == 0)
-execve(buffer, arg, NULL);
+{
+execve(arg[0], arg, NULL);
+perror("execve");
+exit(1);
+}
 else if (pid > 0)
+{
 wait(&stat);
+}
 free(buffer);
 buffer = NULL;
 }
--- main.h
+++ main.h
@@ -4,6 +4,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 #define STDOUT_FILEND 1
 int main(void);
