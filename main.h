65fdb78afeefc566843210f1ef4bcbb53fb1392a
Nazhaaa <nazhakhatib3@gmail.com>
2023-08-25 13:30:32 -0700

    s

--- main.h
+++ main.h
@@ -1,39 +1,68 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_h
+#define _MAIN_h
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <sys/wait.h>
 #include <sys/types.h>
-#include <fcntl.h>
-#include <signal.h>
-#include <errno.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
+#include <limits.h>
+#include <fcntl.h>
+#include <errno.h>
+
+/* read - write buffers */
+#define READ_BUF_SIZE 1024
+#define WRITE_BUF_SIZE 1024
+#define BUF_FLUSH -1
+#define TRUE (1 == 1)
+
+/* my_path function*/
+int my_path(char *xfirst, char **inpt, char **env, int *exit_st);
+
+
+/* env function */
+int print_env(char **env, int *exit_st);
+char *cpy_env(char **env);
+
+/* built- execute functions */
+void built_ins(char *c_lin, char **args, char **env, int *exit_st);
+void exec_env(int status, char **args, int *exit_st, int *taly);
+
+/* strings  functions */
+int str_len(char *s);
+char *str_dup(char *strg);
+int str_cmp(char *str1, char *str2);
+char *str_cpy(char *dstn, char *sourc);
+char *str_cat(char *dstn, char *sourc);
+int put_char(char ch);
+void printstr_(char *st);
+char *path_str(char *p_right, char *xfirst);
+char **_parser(char *l);
+void print_int(int *taly);
+
+
+/* o r functions */
+void no_nul(char *l);
+void grid_free(char **gridd, int _height);
+int char_special(char *buffer, ssize_t byt, int *exit_st);
+
+ssize_t _getline(char **line, size_t *n, FILE *stream);
+char *str_tok(char *str, const char *delimiters);
+void *_realloc(void *ptr, size_t size);
+void *_memcpy(void *destination, const void *src, size_t size);
+int _getsize(const char *str);
+void _puts(const char *str);
+int _putchar(char c);
+
+/*setenv unsetenv*/
+void _setenv(char **arr);
+void _unsetenv(char **arr);
+char *get_env(const char *glob_v);
+char *concatenat(char *glob_v, char *valeur1, char *valeur);
 
 extern char **environ;
 
-#define MCL 100 /*MAX COMMAND LENGTH*/
-#define IBS 64 /*INITIAL BUFFER SIZE*/
-#define BGF 2 /*BUFFER GROWING FACTOR*/
-
-void _prompt(void);
-
-void _tokenize(char *args[], char *cmd);
-void _ext(char *cmd);
-void _printenv(char *cmd);
-/*int _execve(const char *pathname, char *const argv[], char *const envp[]);*/
-/*pid_t _waitpid(pid_t pid, int *wstatus, int options);*/
-/*char *check_cmd(char *cmd);*/
-
-/* string function */
-int _strcmp(const char *s1, const char *s2);
-size_t _strlen(const char *str);
-size_t _strcspn(const char *str, const char *reject);
-char *_strtok(char *str, const char *delim);
-
-/* getline */
-ssize_t _getline(char **lineptr, size_t *n, FILE *stream);
-
 #endif
+
